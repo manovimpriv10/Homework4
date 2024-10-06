290dbefe61c13e2503abc13a44b38872f2f9fa91
Nikita Manov <nikitamanov49@gmail.com>
2024-10-06 19:08:25 +0300

    Add a greeting in three languages

---
+++ hello.pas
@@ -0,0 +1,13 @@
+﻿begin
+  Println('На каком языке Вы говорите ?');
+  Println('1. Французский');
+  Println('2. Английский');
+  Println('3. Итальянский');
+  var a := ReadInteger;
+  Assert((a>0) and (a < 4));
+  case a of
+    1: Println('Bonjour!'); 
+    2: Println('Hello!');   
+    3: Println('Ciao!'); 
+    end;
+end.
